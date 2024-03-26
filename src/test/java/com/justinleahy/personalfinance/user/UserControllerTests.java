fd112f71da10acc6471c4eb40aefd44154b4cc18
Justin Leahy <justinjleahy@gmail.com>
2024-03-26 12:36:03 -0400

    Reworked tests to incorporate toJSON() and assertions

--- src/test/java/com/justinleahy/personalfinance/user/UserControllerTests.java
+++ src/test/java/com/justinleahy/personalfinance/user/UserControllerTests.java
@@ -1,12 +1,19 @@
 package com.justinleahy.personalfinance.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,74 +23,98 @@ public class UserControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+    private static final Logger log = LoggerFactory.getLogger(UserControllerTests.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     @Test
     void testPostUser() throws Exception {
-        String userJson = "{" +
-                    "\"firstName\": \"John\"," +
-                    "\"lastName\" : \"Doe\"," +
-                    "\"userName\" : \"johndoe100\"," +
-                    "\"passwordHash\": \"hashedpassword\"" +
-                "}";
+        User newUser = new User("John", "Doe", "johndoe100", "hashedpassword");
 
-        mockMvc.perform(post("/user")
+        MvcResult postResult = mockMvc.perform(post("/user")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(userJson)
+                        .content(newUser.toJSON())
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated()).andReturn();
+
+        User userPost = objectMapper.readValue(postResult.getResponse().getContentAsString(), User.class);
+        log.info("POST User response: {}", userPost);
+
+        assertEquals(newUser.getFirstName(), userPost.getFirstName());
+        assertEquals(newUser.getLastName(), userPost.getLastName());
+        assertEquals(newUser.getUserName(), userPost.getUserName());
+        assertEquals(newUser.getPasswordHash(), userPost.getPasswordHash());
     }
 
     @Test
     void testGetUser() throws Exception {
-        String userJson = "{" +
-                    "\"firstName\": \"John\"," +
-                    "\"lastName\" : \"Doe\"," +
-                    "\"userName\" : \"johndoe101\"," +
-                    "\"passwordHash\": \"hashedpassword\"" +
-                "}";
+        User newUser = new User("John", "Doe", "johndoe101", "hashedpassword");
 
-        mockMvc.perform(post("/user")
+        MvcResult postResult = mockMvc.perform(post("/user")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(userJson)
+                        .content(newUser.toJSON())
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated()).andReturn();
 
-        mockMvc.perform(get("/user/{id}", 1))
-                .andExpect(status().isOk());
+        User userPost = objectMapper.readValue(postResult.getResponse().getContentAsString(), User.class);
+        log.info("POST user response: {}", userPost);
+
+        MvcResult getResult = mockMvc.perform(get("/user/{id}", userPost.getId()))
+                .andExpect(status().isOk()).andReturn();
+
+        User userGet = objectMapper.readValue(getResult.getResponse().getContentAsString(), User.class);
+        log.info("GET user response: {}", userGet);
+
+        assertEquals(userPost.getFirstName(), userGet.getFirstName());
+        assertEquals(userPost.getLastName(), userGet.getLastName());
+        assertEquals(userPost.getUserName(), userGet.getUserName());
+        assertEquals(userPost.getPasswordHash(), userGet.getPasswordHash());
     }
 
     @Test
     void testUpdateUser() throws Exception {
-        String userJson = "{" +
-                    "\"firstName\": \"John\"," +
-                    "\"lastName\" : \"Doe\"," +
-                    "\"userName\" : \"johndoe102\"," +
-                    "\"passwordHash\": \"hashedpassword\"" +
-                "}";
+        User newUser = new User("John", "Doe", "johndoe102", "hashedpassword");
 
-        mockMvc.perform(post("/user")
+        MvcResult postResult = mockMvc.perform(post("/user")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(userJson)
+                        .content(newUser.toJSON())
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated()).andReturn();
 
-        userJson = "{" +
-                    "\"firstName\": \"Updated\"," +
-                    "\"lastName\" : \"User\"," +
-                    "\"userName\" : \"johndoe102\"," +
-                    "\"passwordHash\": \"hashedpassword\"" +
-                "}";
+        User userPost = objectMapper.readValue(postResult.getResponse().getContentAsString(), User.class);
+        log.info("POST user response: {}", userPost);
 
-        mockMvc.perform(patch("/user/{id}", 1)
+        newUser.setFirstName("Updated");
+        newUser.setLastName("User");
+
+        MvcResult patchResult = mockMvc.perform(patch("/user/{id}", userPost.getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(userJson)
+                        .content(newUser.toJSON())
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).andReturn();
+
+        User userPatch = objectMapper.readValue(patchResult.getResponse().getContentAsString(), User.class);
+        log.info("PATCH user response: {}", userPatch);
+
+        assertNotEquals(userPost.getFirstName(), userPatch.getFirstName());
+        assertNotEquals(userPost.getLastName(), userPatch.getLastName());
+        assertEquals(newUser.getUserName(), userPatch.getUserName());
+        assertEquals(newUser.getPasswordHash(), userPatch.getPasswordHash());
     }
 
     @Test
     void testDeleteUser() throws Exception {
-        mockMvc.perform(delete("/user/{id}", 1))
+        User newUser = new User("John", "Doe", "johndoe103", "hashedpassword");
+
+        MvcResult postResult = mockMvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newUser.toJSON())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated()).andReturn();
+
+        User userPost = objectMapper.readValue(postResult.getResponse().getContentAsString(), User.class);
+        log.info("POST user response: {}", userPost);
+
+        mockMvc.perform(delete("/user/{id}", userPost.getId()))
                 .andExpect(status().isNoContent());
     }
 }
